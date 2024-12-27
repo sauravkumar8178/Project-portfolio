b0d3e8f0eb82b04c86f50c2764e63bc05dc282c9
Saurav Kumar <ss989108@gmail.com>
2024-12-27 21:25:05 +0530

    fix links

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -26,12 +26,8 @@ function SocialLinks() {
     <div className="flex text-lg gap-3.5 float-right transition-opacity duration-300 hover:opacity-90">
       <SocialLink href={socialLinks.twitter} icon={FaXTwitter} />
       <SocialLink href={socialLinks.github} icon={FaGithub} />
-      <SocialLink href={socialLinks.instagram} icon={FaInstagram} />
       <SocialLink href={socialLinks.linkedin} icon={FaLinkedinIn} />
       <SocialLink href={socialLinks.email} icon={TbMailFilled} />
-      <a href="/rss.xml" target="_self">
-        <FaRss />
-      </a>
     </div>
   );
 }
